021a9e0640183c9893ba4028562ae6b2ce95bc87
Tedd Mason <teddmason@gmail.com>
2019-03-26 11:30:21 +0000

    debug work

--- server/services/web-socket.js
+++ server/services/web-socket.js
@@ -13,6 +13,9 @@ module.exports = {
     if (connected) {
       console.log('Publishing warnings to sockets')
       const fwis = new Fwis(await fwisService.get())
+      console.log('Warning count: ' + fwis.data.warnings.length)
+      console.log(JSON.stringify(fwis.data))
+      console.log(JSON.stringify(fwis.summaryData))
       server.publish('/summary', {
         params: fwis.getSummaryTable(),
         updateTime: new Date().toISOString()
