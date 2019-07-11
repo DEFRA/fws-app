e3a9a1007f5612055fb47717cc4c1e06a8b6875a
David Stone <davidjamesstone@gmail.com>
2019-07-11 16:36:45 +0100

    Remove web socket

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const hapi = require('hapi')
-// const schedule = require('node-schedule')
 const config = require('./config')
-// const ws = require('./services/web-socket')
 
 async function createServer () {
   // Create the hapi server
@@ -21,21 +19,11 @@ async function createServer () {
   await server.register(require('./plugins/views'))
   await server.register(require('./plugins/router'))
   await server.register(require('./plugins/error-pages'))
-  // await server.register(require('./plugins/socket'))
 
   if (config.isDev) {
     await server.register(require('blipp'))
     await server.register(require('./plugins/logging'))
   }
-
-  // Initialise websocket connection
-  // ws.init(server)
-
-  // // Publish warning data every minute
-  // schedule.scheduleJob('* * * * *', () => {
-  //   console.log('Schedule warnings')
-  //   // ws.publishWarnings(server)
-  // })
 
   return server
 }
