706b6426b5cdbc9424ae19336a56e4ff4627a977
David Stone <davidjamesstone@gmail.com>
2019-07-19 14:04:43 +0100

    Try logout url

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -42,7 +42,7 @@ module.exports = [{
   options: {
     handler: function (request, h) {
       request.cookieAuth.clear()
-      return h.redirect(`https://login.microsoftonline.com/${config.adTenant}/oauth2/v2.0/logout?post_logout_redirect_uri=${config.homePage}`)
+      return h.redirect(`https://login.microsoftonline.com/526e1181-9d1f-4f48-bbed-4b0d2f09c645/oauth2/v2.0/logout?post_logout_redirect_uri=${config.homePage}`)
     }
   }
 }]
