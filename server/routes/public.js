e604be870b13472f18433c05c7f294f342f2d2e3
John Shields <John.Shields@environment-agency.gov.uk>
2020-05-14 14:39:24 +0100

    FIS-491: Add status and robots.txt routes.

--- server/routes/public.js
+++ server/routes/public.js
@@ -19,4 +19,12 @@ module.exports = [{
       }
     }
   }
+}, {
+  method: 'GET',
+  path: '/robots.txt',
+  options: {
+    handler: {
+      file: 'server/public/static/robots.txt'
+    }
+  }
 }]
