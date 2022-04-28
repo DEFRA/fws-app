7757207a9b2e5bb8f09b02d98ddd1d3d0e4d52a7
LeeGordon83 <lee.gordon83@gmail.com>
2022-04-28 10:21:25 +0100

    refactored unit test

--- test/routes/index-training.js
+++ test/routes/index-training.js
@@ -16,40 +16,15 @@ lab.experiment(('Training Env banner'), () => {
     composeServer.stop()
   })
 
-  const urls = [{
-    url: '/',
-    code: 200,
-    text: [
-      'This application version is for training only'
-    ]
-  }, {
-    url: '/',
-    code: 200,
-    text: [
-      'govuk-header-blue'
-    ]
-  }, {
-    url: '/',
-    code: 200,
-    text: [
-      'govuk-footer-blue'
-    ]
-  }]
-
-  urls.forEach(item => {
-    lab.test(`Training banner test: ${item.url} returns ${item.code}`, async () => {
-      const response = await server.inject({
-        method: item.method || 'GET',
-        url: item.url,
-        auth: item.auth,
-        payload: item.payload
-      })
-      Code.expect(response.statusCode).to.equal(item.code)
-      if (item.text && item.text.length > 0) {
-        item.text.forEach(text => {
-          Code.expect(response.payload).to.include(text)
-        })
-      }
+  lab.test('trainning env', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/'
     })
+
+    Code.expect(response.payload).to.contain('This application version is for training only')
+    Code.expect(response.payload).to.contain('govuk-header-blue')
+    Code.expect(response.payload).to.contain('govuk-footer-blue')
+    Code.expect(response.statusCode).to.equal(200)
   })
 })
