5e2126ecc35c155fdf778182dd38f2c2f6d59dc7
pwadmore-ea <8915039+pwadmore-ea@users.noreply.github.com>
2025-08-27 10:48:59 +0100

    Replace @hapi/joi with joi

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-const Joi = require('@hapi/joi')
+const Joi = require('joi')
 
 // Define config schema
 const schema = Joi.object({
--- server/routes/target-area.js
+++ server/routes/target-area.js
@@ -1,4 +1,4 @@
-const joi = require('@hapi/joi')
+const joi = require('joi')
 const boom = require('@hapi/boom')
 const TargetAreaView = require('../models/target-area-view')
 
